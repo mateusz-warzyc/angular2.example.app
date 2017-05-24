5dc1d48143f8a978c203eaa7c1258e6e71b179d5
Vernathic <vernathic@gmail.coml>
2017-05-24 18:10:04 +0200

    initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavbarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ NavbarComponent ,EventsListComponent,EventThumbnailComponent, EventsAppComponent ],
+    providers: [ EventService, ToastrService ],
+    bootstrap: [EventsAppComponent ]
+})
+export class AppModule {}
